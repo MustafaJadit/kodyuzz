a8b215ae20ec8a3adc4745d20894469ff2481251
mustafaatush <mustafaatush@gmail.com>
2020-06-25 13:15:35 -0700

    completed downloadManager

---
+++ app/src/main/java/com/kodyuzz/tafa/DownloadManagerActivity.java
@@ -0,0 +1,84 @@
+package com.kodyuzz.tafa;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class DownloadManagerActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private long downloadId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerReceiver(onDownloadComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+        beginDownload("http://www.tutorialspoint.com/java/java_tutorial.pdf");
+
+    }
+
+    private BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            if (downloadId == id) {
+                Toast.makeText(context, "completed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(onDownloadComplete);
+    }
+
+    private void beginDownload(String file_link) {
+        File file = new File(getExternalFilesDir(null), "Tam2");
+        //checking if android version is equal and greater than noughat
+
+        //now if download complete file not visible now lets show it
+        DownloadManager.Request request = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            request = new DownloadManager.Request(Uri.parse(file_link))
+                    .setTitle("Mustafa")
+                    .setDescription("Downloading")
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setDestinationUri(Uri.fromFile(file))
+                    .setRequiresCharging(false)
+                    .setAllowedOverMetered(true)
+                    .setAllowedOverRoaming(true);
+        } else {
+            request = new DownloadManager.Request(Uri.parse(file_link))
+                    .setTitle("Mustafa")
+                    .setDescription("Downloading")
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setDestinationUri(Uri.fromFile(file))
+                    .setAllowedOverRoaming(true);
+        }
+
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        downloadId = downloadManager.enqueue(request);
+
+
+    }
+
+}
