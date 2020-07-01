55969c5ce59a56734ef92195d29a3c7ce052f5e7
mustafaatush <mustafaatush@gmail.com>
2020-07-01 15:59:22 -0700

    coding challenge + kotlin android lib + add kotlin to existing module

--- app/src/main/java/com/kodyuzz/tafa/student/MainActivity.kt
+++ app/src/main/java/com/kodyuzz/tafa/student/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.kodyuzz.tafa.student
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         var staticVar1: String = "d"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        println(this@MainActivity)
+        var view = TextView(this)
+        view.setOnClickListener {
+            View.OnClickListener { }
+        }
     }
 }
---
+++ lib/src/main/java/com/kodyuzz/lib/PureKotlinTraining2.kt
@@ -0,0 +1,84 @@
+package com.kodyuzz.lib
+
+fun main() {
+
+//    forLoopNameDemo()
+
+//    regularArrayDemo()
+
+//    listDemo()
+
+//    mapDemo()
+
+    defineLambda()
+}
+
+fun defineLambda() {
+    printFunction()
+    myLambdaFunction2
+    myLambdaFunction
+    myLambdaFunction3
+    println(myLambdaFunction4)
+}
+
+fun printFunction() {
+
+}
+
+val myLambdaFunction2: () -> Unit = {}
+
+val myLambdaFunction: (String) -> Unit =
+        { v ->
+            println(v)
+        }
+
+val myLambdaFunction3: (String) -> Unit =
+        { value2 ->
+            String
+            println(value2)
+        }
+
+val myLambdaFunction4: (s: String) -> Int =
+        { g ->
+            println()
+            g.length
+        }
+
+
+fun mapDemo() {
+    var hashMapOf = hashMapOf("a" to 1, "b" to 2)
+    var mutableMapOf = mutableMapOf("a" to 2)
+    var mapOf = mapOf("m1" to 1)
+    var plus = mapOf.plus("b" to 2)
+}
+
+fun listDemo() {
+    var listOf = arrayListOf("a", "b")
+    var mutableListOf = mutableListOf("s1", "s2")
+    var e = kotlin.collections.listOf<String>()
+    var e2 = emptyList<String>()
+
+
+}
+
+fun regularArrayDemo() {
+    var a1 = IntArray(3, { i -> i * 2 })
+    var a2 = IntArray(3) { i -> i * 2 }
+    a1.first()
+    a1.last()
+    a1.get(2)
+    a1.set(1, 322)
+    a1.sort()
+
+}
+
+fun forLoopNameDemo() {
+    outer@ for (i in 1..10) {
+        for (j in 'a'..'z') {
+            if (j == 'v') continue@outer
+            print(j + "\t")
+        }
+        println("-------------")
+        println("-------------")
+    }
+}
